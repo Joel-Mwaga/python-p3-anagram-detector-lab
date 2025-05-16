0d3693f64bb90a0a870a42d7ad96641f95bae36e
Joel Mwaga <mwagahjoel14@gmail.com>
2025-05-16 17:17:14 +0300

    changed 1 file

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()  
+    
+    def match(self, possible_anagrams):
+        sorted_word = sorted(self.word)  
+        matches = []
+        
+        for candidate in possible_anagrams:
+            
+            if sorted_word == sorted(candidate.lower()):
+                matches.append(candidate)
+        
+        return matches
